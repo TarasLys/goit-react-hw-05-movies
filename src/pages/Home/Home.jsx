d2431ba3228d7ab97d7cffddd3fd92049ff5dd84
TarasLys <opros0609@gmail.com>
2023-12-25 23:12:54 +0200

    777

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import css from './Home.module.css';
 import { fetchMoviesDay } from 'service/service';
 import MovieList from 'components/MovieList/MovieList';
